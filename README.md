675dd977e70a9cb98c7f5666438101e2e539a783
Matija Stepanic <stepanic.matija@gmail.com>
2021-06-30 07:03:21 +0200

    Updated README and release script

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # microblink-license-usage-logs-uploader
 
 Microblink License Usage log's client for sending logs to the Microblink License Usage log's service
+
+## Docker
+
+Application is bundled as Docker Image for the easier deployment.
+
+### Releasing levels
+
+1. `Private` at Google Cloud registry
+  - only for internal testing
+
+2. `Protected` at Microblink private registry
+  - private release for security patches
+
+3. `Public` at Docker Hub
+  - public release which is internally tested and it could be deployed by everyone
+### Build & Push
+
+- `PRIVATE_RELEASE` - push Docker image to private Google Cloud Registry
+- `PROTECTED_RELEASE` - push Docker image to private Microblink registry for sharing protected images internally and with the clients for images which are not allowed to be publicly available at Docker Hub
+- `PUBLIC_RELEASE` - push Docker image to public Docker Hub repository
+- `SOURCE_PRIVATE_PUSH` - push source to private Bitbucket repository
+- `SOURCE_PUBLIC_PUSH` - push source to public GitHub repository
+
+```bash
+PRIVATE_RELEASE=true PROTECTED_RELEASE=true PUBLIC_RELEASE=true SOURCE_PRIVATE_PUSH=true SOURCE_PUBLIC_PUSH=true ./build-and-push.sh
+```
--- build-and-push.sh
+++ build-and-push.sh
@@ -5,25 +5,66 @@ source ./build.sh
 # Private
 DOCKER_IMAGE_GCR="eu.gcr.io/blinkid-verify/license-usage-logs-uploader-private"
 
-for DOCKER_TAG in "${DOCKER_TAGS[@]}"
-do
-	docker tag "$DOCKER_IMAGE:$DOCKER_TAG" "$DOCKER_IMAGE_GCR:$DOCKER_TAG"
-	echo "docker push $DOCKER_IMAGE_GCR:$DOCKER_TAG"
-	docker push "$DOCKER_IMAGE_GCR:$DOCKER_TAG"
-done
+if [ -z ${PRIVATE_RELEASE+x} ];
+then
+  echo "PRIVATE release skipped!";
+else
+  for DOCKER_TAG in "${DOCKER_TAGS[@]}"
+  do
+    docker tag "$DOCKER_IMAGE:$DOCKER_TAG" "$DOCKER_IMAGE_GCR:$DOCKER_TAG"
+    echo "docker push $DOCKER_IMAGE_GCR:$DOCKER_TAG"
+    docker push "$DOCKER_IMAGE_GCR:$DOCKER_TAG"
+  done
+fi
+
+# Protected
+DOCKER_IMAGE_MICROBLINK="docker.microblink.com/license-usage-logs-uploader"
+
+if [ -z ${PROTECTED_RELEASE+x} ];
+then
+  echo "PROTECTED release skipped!";
+else
+  for DOCKER_TAG in "${DOCKER_TAGS[@]}"
+  do
+    docker tag "$DOCKER_IMAGE:$DOCKER_TAG" "$DOCKER_IMAGE_MICROBLINK:$DOCKER_TAG"
+    echo "docker push $DOCKER_IMAGE_MICROBLINK:$DOCKER_TAG"
+    docker push "$DOCKER_IMAGE_MICROBLINK:$DOCKER_TAG"
+  done
+fi
 
 # Public
 DOCKER_IMAGE_DOCKER_HUB="microblink/license-usage-logs-uploader"
-for DOCKER_TAG in "${DOCKER_TAGS[@]}"
-do
-	docker tag "$DOCKER_IMAGE:$DOCKER_TAG" "$DOCKER_IMAGE_DOCKER_HUB:$DOCKER_TAG"
-	echo "docker push $DOCKER_IMAGE_DOCKER_HUB:$DOCKER_TAG"
-	docker push "$DOCKER_IMAGE_DOCKER_HUB:$DOCKER_TAG"
-done
+
+if [ -z ${PUBLIC_RELEASE+x} ];
+then
+  echo "PUBLIC release skipped!";
+else
+  for DOCKER_TAG in "${DOCKER_TAGS[@]}"
+  do
+    docker tag "$DOCKER_IMAGE:$DOCKER_TAG" "$DOCKER_IMAGE_DOCKER_HUB:$DOCKER_TAG"
+    echo "docker push $DOCKER_IMAGE_DOCKER_HUB:$DOCKER_TAG"
+    docker push "$DOCKER_IMAGE_DOCKER_HUB:$DOCKER_TAG"
+  done
+fi
 
 # Set tag
-git tag "v$VERSION"
-git push --tags
+git tag -f "v$VERSION"
+
+# Source - private
+if [ -z ${SOURCE_PRIVATE_PUSH+x} ];
+then
+  echo "Source private push skipped!";
+else
+  git push -f origin --tags
+fi
+
+# Source - public
+if [ -z ${SOURCE_PUBLIC_PUSH+x} ];
+then
+  echo "Source public push skipped!";
+else
+  git push -f github --tags
+fi
 
 echo "------- BUILD & PUSH DONE -------"
 echo ""
